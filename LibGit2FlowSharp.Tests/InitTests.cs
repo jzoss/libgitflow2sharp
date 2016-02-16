085a32308a99fc7700658beb2c11caae7cae012f
Trond <trond@platekompaniet.no>
2016-02-16 10:39:29 +0100

    Some more simple tests

--- LibGit2FlowSharp.Tests/InitTests.cs
+++ LibGit2FlowSharp.Tests/InitTests.cs
@@ -11,14 +11,34 @@ namespace LibGit2FlowSharp.Tests
 {
     public class InitTests
     {
+        private Flow _testFlow;
+        private const string Testpath = @"c:\test";
+
+        public InitTests()
+        {   
+            // could do the          
+        }
 
         [Fact]
-        public void FolderIsGitRepository()
+        public void TestFolderIsAGitRepository() 
         {
-            using (var repo = new Repository(@"c:\test"))
+            using (var repo = new Repository(Testpath))
             {
-                
             }
+        }
+
+        [Fact]
+        public void FlowSetsRepository()
+        {
+            _testFlow = new Flow(new Repository(Testpath));
+            Assert.NotNull(_testFlow.Repository);            
+        }
+
+        [Fact]
+        public void FlowIsInitialized()
+        {
+            _testFlow = new Flow(new Repository(Testpath));
+            Assert.True(_testFlow.IsInitialized());
         }
     }
 }
